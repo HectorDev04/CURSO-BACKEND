b8cedb4fe9249dc10c3a83e5519d11a02afbe534
HectorDev <hectorrodriguezzzz04@gmail.com>
2024-05-14 15:14:27 -0600

    Agregando ejercicios de la clase 4

---
+++ Clase-4/ejercicio3.js
@@ -0,0 +1,9 @@
+function multiplicar(a, b, callback) {
+  const resultado = a * b;
+
+  callback(resultado);
+}
+
+multiplicar(6, 3, (resultado) => {
+  console.log(`resultado :`, resultado);
+});
---
+++ Clase-4/ejercicio5.js
@@ -0,0 +1,9 @@
+function operacionAsync(callback) {
+  setTimeout(() => {
+    callback("operacion async completada");
+  }, 2000);
+}
+
+operacionAsync((mensaje) => {
+  console.log(mensaje);
+});
